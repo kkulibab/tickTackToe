0f0b863757172387b15c26278d33765fbd5900d0
KK <krzysztof.kulibab@gmail.com>
2021-02-18 14:16:15 +0100

    Tutorial 9 - Error handling

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ game = [[0, 0, 0],
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
-    print("   0  1  2")
-    if not just_display:
-        game_map[row][column] = player
-    for count, row in enumerate(game_map):
-        print(count, row)
-    return game_map
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+
+    except IndexError as e:
+        print("Error: make sure you input row/column as 0, 1 or 2", e)
+
+    except Exception as e:
+        print("Something went wrong", e)
 
 
 game = game_board(game, just_display=True)
