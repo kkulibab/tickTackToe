33b7fcf39f6f5974d05efa84a2de8cb928c035aa
KK <krzysztof.kulibab@gmail.com>
2021-02-18 13:56:31 +0100

    Tutorial 8

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ game = [[0, 0, 0],
         [0, 0, 0]]
 
 
-def game_board(player=0, row=0, column=0,just_display=False):
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
     print("   0  1  2")
     if not just_display:
-        game[row][column] = player
-    for count, row in enumerate(game):
+        game_map[row][column] = player
+    for count, row in enumerate(game_map):
         print(count, row)
+    return game_map
 
 
-#game[0][1] = 1
-
-game_board(just_display=True)
-game_board(player=1, row=2, column=1)
+game = game_board(game, just_display=True)
+game = game_board(game, player=1, row=2, column=1)
