05e34cb0569dc363e46f819ccb9ec4c39224ef68
KK <krzysztof.kulibab@gmail.com>
2021-02-19 14:31:50 +0100

    Tutorial 11 - vertical win

--- main.py
+++ main.py
@@ -19,11 +19,21 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
         print("Something went wrong", e)
 
 
-def win(current_game):
+def vertical_win(current_game):
+    for col in range(len(current_game)):
+        check = []
+        for row in game:
+            check.append(row)
+
+        if check.count(check[0]) == len(check) and check[0] != 0:
+            print("Winner")
+
+
+def horizontal_win(current_game):
     for row in current_game:
         print(row)
         if row.count(row[0]) == len(row) and row[0] != 0:
-            print("winner")
+            print("Winner")
 
 
 game = game_board(game, just_display=True)
